3f37aaf2903b6c254da21a06f37e6b9025933f68
WIN11 <robbin0919@gmail.com>
2024-07-29 00:27:37 +0800

    modified:   Overloading/Overloading.csproj  	modified:   Overloading/Program.cs  	modified:   OverloadingLib/Class1.cs

--- Overloading/Program.cs
+++ Overloading/Program.cs
@@ -1,8 +1,16 @@
-﻿namespace Overloading;
+﻿using OverloadingLib;
+namespace Overloading;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Circle circle = new Circle { Radius = 5 };
+        Rectangle rectangle = new Rectangle { Length = 4, Width = 3 };
+        Triangle triangle = new Triangle { Base = 6, Height = 8 };
+
+        Console.WriteLine("圓形面積：" + circle.CalculateArea());   
+        Console.WriteLine("矩形面積：" + rectangle.CalculateArea());
+        Console.WriteLine("三角形面積：" + triangle.CalculateArea());
     }
 }
--- OverloadingLib/Class1.cs
+++ OverloadingLib/Class1.cs
@@ -1,5 +1,37 @@
 ﻿namespace OverloadingLib;
-public class Class1
+public abstract class Shape
 {
+    public abstract double CalculateArea();
+}
 
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Length * Width;
+    }
+}
+
+public class Triangle : Shape
+{
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return 0.5 * Base * Height;
+    }
 }
