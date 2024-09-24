ec683a4993e5f14ff97d6458c41fbadb21d73f61
WIN11 <robbin0919@gmail.com>
2024-09-25 00:35:32 +0800

    modified:   MariaDB_ADO_NET_01.csproj  	new file:   MariaDB_ADO_NET_01.sln  	modified:   Program.cs

--- MariaDB_ADO_NET_01/Program.cs
+++ MariaDB_ADO_NET_01/Program.cs
@@ -1,8 +1,32 @@
-﻿namespace MariaDB_ADO_NET_01;
-class Program
-{
-    static void Main(string[] args)
+﻿
+using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+namespace MariaDB_ADO_NET_01;
+ 
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            string connectionString = "server=your_server;database=your_database;uid=your_user;pwd=your_password;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM your_table", connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["column1"] + " " + reader["column2"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
     }
-}
+ 
