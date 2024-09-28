67bbe09c7df908f82862cb32640844ab45ad9853
WIN11 <robbin0919@gmail.com>
2024-09-29 00:45:20 +0800

    modified:   .gitignore new   AspNetCoreInDocker01 new   AspNetCoreInDocker02

---
+++ AspNetCoreInDocker01/Models/Log.cs
@@ -0,0 +1,5 @@
+public class Log
+{
+    public DateTime Timestamp { get; set; }
+    public string Message { get; set; }
+}
---
+++ AspNetCoreInDocker01/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(); // 添加授權服務
+builder.Services.AddControllersWithViews();
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
