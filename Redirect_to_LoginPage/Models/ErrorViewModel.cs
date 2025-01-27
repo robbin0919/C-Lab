ea590b68ec2fd452c371f03ad438f923620290c2
ASUS-B2502-TWM <robbin0919@gmail.com>
2025-01-27 23:07:56 +0800

    modified:   .gitignore new file:  Redirect_to_LoginPage

---
+++ Redirect_to_LoginPage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Redirect_to_LoginPage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
