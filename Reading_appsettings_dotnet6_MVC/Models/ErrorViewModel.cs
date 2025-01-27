f2913005dd365d29f23b3f08d6debfd3581b4e92
ASUS-B2502-TWM <robbin0919@gmail.com>
2025-01-27 19:32:51 +0800

    modified:   .gitignore new  Reading_appsettings_dotnet6_MVC

---
+++ Reading_appsettings_dotnet6_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Reading_appsettings_dotnet6_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
